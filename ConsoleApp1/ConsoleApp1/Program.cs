286244ec47d4c49b66628428c565cd3596f294ff
Антонов Антон <antonov.anton@outlook.com>
2018-04-27 21:02:19 +0300

    Add SCode

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = (int)+(char)-(int)+(long)-1;
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
